bb3879dcd9e353548a50026954e376b77f39e7ca
chris3evans <chris3evans.ce@gmail.com>
2021-07-07 18:10:35 +0100

    Receive and handle photo data - no photo rendering

--- Resources/Javascript/model.js
+++ Resources/Javascript/model.js
@@ -1,26 +1,46 @@
+const fileInput = document.querySelector(".files");
+
 export let state = [];
+export let filePathArr = [];
 
 export const getLocalStorage = function () {
-  const storage = localStorage.getItem('Country Entries')
+  const storage = localStorage.getItem("Country Entries");
   if (storage) state = JSON.parse(storage);
-}
+};
 
 export const setLocalStorage = function () {
-  localStorage.setItem('Country Entries', JSON.stringify(state))
-}
+  localStorage.setItem("Country Entries", JSON.stringify(state));
+};
 
 export const loadLocalStorage = function () {
   getLocalStorage();
-}
+};
 
 export const deleteLocalStorage = function () {
-  localStorage.removeItem('Country Entries');
-}
+  localStorage.removeItem("Country Entries");
+};
 
-export const formatNewEntry = function (newEntry, countryData) {
+export const getPhotoData = function () {
+  fileInput.onchange = function () {
+    const fileObject = fileInput.files;
+    const fileArr = Object.values(fileObject);
+
+    fileArr.forEach(function (file) {
+      if (file.type === "image/jpeg" || file.type === "image/png") {
+        filePathArr.push(file.webkitRelativePath);
+      }
+    });
+  };
+};
+
+export const formatNewEntry = function (newEntry, countryData, photoData) {
   // Checks data was actually entered
-  if (newEntry.country === '' || newEntry.nameTag === '' || newEntry.address === '') {
-    console.log('Please fill out all form fields');
+  if (
+    newEntry.country === "" ||
+    newEntry.nameTag === "" ||
+    newEntry.address === ""
+  ) {
+    console.log("Please fill out all form fields");
   } else {
     // Checks input data is correct
     const newEntryNormal = normalizeFormInputs(newEntry);
@@ -28,15 +48,20 @@ export const formatNewEntry = function (newEntry, countryData) {
     // Formats form data object
     let newEntryFormat = {
       country: newEntryNormal.country,
-      locations: [{
-        nameTag: newEntryNormal.nameTag,
-        locationAddress: newEntryNormal.address,
-        startDate: newEntryNormal.startDate,
-        endDate: newEntryNormal.endDate,
-      }],
-    }
+      locations: [
+        {
+          nameTag: newEntryNormal.nameTag,
+          locationAddress: newEntryNormal.address,
+          startDate: newEntryNormal.startDate,
+          endDate: newEntryNormal.endDate,
+          photos: photoData,
+        },
+      ],
+    };
 
     newEntryFormat.timesVisited = newEntryFormat.locations.length;
+
+    // Receive and store photo data
 
     // Check if country entry already exists
     const match = countryData.find(function (country) {
@@ -45,59 +70,64 @@ export const formatNewEntry = function (newEntry, countryData) {
 
     // If country entry does exist and there is data in the state
     if (match && state.length !== 0) {
-        // Nested object containing specific location data
-        const newData = formatForExistingEntry(newEntryFormat);
+      // Nested object containing specific location data
+      const newData = formatForExistingEntry(newEntryFormat);
 
-        // Push this data to the country's location array
-        match.locations.push(newData);
+      // Push this data to the country's location array
+      match.locations.push(newData);
 
-        // Update times visited value
-        match.timesVisited = match.locations.length;
-      
-        // Save to local storage
-        setLocalStorage();
+      // Update times visited value
+      match.timesVisited = match.locations.length;
 
-    // If country entry does NOT exist
+      // Save to local storage
+      setLocalStorage();
+
+      // If country entry does NOT exist
     } else {
       // Push entire new country object to state
       state.push(newEntryFormat);
+      console.log(newEntryFormat);
+      console.log(state);
 
       // Save to local storage
       setLocalStorage();
     }
     console.log(state);
   }
-}
+};
 
 const formatForExistingEntry = function (newEntry) {
-  const location = Object.entries(newEntry).filter(function(entry) {
-    if (entry[0].startsWith('locations')) {
-      return entry
-    }
-  }).shift();
+  const location = Object.entries(newEntry)
+    .filter(function (entry) {
+      if (entry[0].startsWith("locations")) {
+        return entry;
+      }
+    })
+    .shift();
 
   const locationDataArr = location.pop();
-  const [locationData] = locationDataArr
+  const [locationData] = locationDataArr;
   return locationData;
-}
+};
 
 const normalizeFormInputs = function (inputData) {
   // inputData = initial object from form data
   if (inputData.country.length !== 0 && inputData.nameTag.length !== 0) {
     // Normalize country name input
-    const countryCapitalised = capitalise(inputData.country)
+    const countryCapitalised = capitalise(inputData.country);
     const nameTagCapitalised = capitalise(inputData.nameTag);
     inputData.nameTag = nameTagCapitalised;
     inputData.country = countryCapitalised;
     return inputData;
   }
-}
+};
 
 // Capitalisation functionality
 const capitalise = function (string) {
-  const words = string.split(' ');
-  return words.map(function(word) {
-    return word[0].toUpperCase() + word.toLowerCase().slice(1);
-  })
-  .join(' ');
-}
+  const words = string.split(" ");
+  return words
+    .map(function (word) {
+      return word[0].toUpperCase() + word.toLowerCase().slice(1);
+    })
+    .join(" ");
+};
